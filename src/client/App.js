170e4c37673da0788dd633497a942d2d4fb95490
jsmithb117 <jsmithb117@gmail.com>
2021-02-01 11:32:56 -0800

    Webpack, babel, react set up complete.

---
+++ src/client/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+var App = () => <div className="Hello">We're gonna do some Chickin stuff</div>;
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            { test: /\.(js)$/, use: 'babel-loader' },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.html$/, use: [{ loader: "html-loader" }] }
+        ],
+
+    },
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: "./index.html"
+        })
+    ],
+    devServer: {
+        port: 9100
+    }
+}
